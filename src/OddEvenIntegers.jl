e4a7ba5d311e5215c478b8ca81cec5c5c71d6497
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-02-27 11:30:04 +0400

    rename package to OddEvenIntegers.jl

---
+++ src/OddEvenIntegers.jl
@@ -0,0 +1,104 @@
+module OddEvenIntegers
+
+using HalfIntegers
+
+export Odd, Even
+
+"""
+	OddInteger  <: Integer
+
+Abstract superptye of odd integer types.
+Currently, only `Odd <: OddInteger` is implemented.
+"""
+abstract type AbstractOddEvenInteger <: Integer end
+
+"""
+	Odd{T<:Integer} <: AbstractOddEvenInteger
+
+Represent an odd integer.
+"""
+struct Odd{T<:Integer} <: AbstractOddEvenInteger
+	x :: T
+
+	Odd{T}(x::T) where {T} = (@assert isodd(x); new(x))
+end
+
+Odd(x::Integer) = Odd{typeof(x)}(x)
+Odd(x::Odd) = x
+
+"""
+	Even{T<:Integer} <: AbstractOddEvenInteger
+
+Represent an even integer.
+"""
+struct Even{T<:Integer} <: AbstractOddEvenInteger
+	x :: T
+
+	Even{T}(x::T) where {T} = (@assert iseven(x); new(x))
+end
+Even(x::Integer) = Even{typeof(x)}(x)
+Even(x::Even) = x
+
+Base.promote_type(::Type{Odd{A}}, ::Type{Odd{B}}) where {A,B} = promote_type(A,B)
+Base.promote_type(::Type{Even{A}}, ::Type{Even{B}}) where {A,B} = promote_type(A,B)
+Base.promote_rule(::Type{Odd{A}}, ::Type{T}) where {A,T<:Real} = promote_type(A, T)
+Base.promote_rule(::Type{Even{A}}, ::Type{T}) where {A,T<:Real} = promote_type(A, T)
+
+for f in [:AbstractFloat, :Float16, :Float32, :Float64, :BigFloat,
+		:Int8, :Int16, :Int32, :Int64, :Int128, :BigInt]
+	@eval Base.$f(x::AbstractOddEvenInteger) = $f(x.x)
+end
+
+for f in (:<, :(<=), :rem, :div)
+	@eval Base.$f(x::AbstractOddEvenInteger, y::AbstractOddEvenInteger) = $f(x.x, y.x)
+end
+for f in (:trailing_zeros,)
+	@eval Base.$f(x::AbstractOddEvenInteger) = $f(x.x)
+end
+for f in (:(>>), :(<<))
+	@eval Base.$f(x::AbstractOddEvenInteger, y::UInt) = $f(x.x, y)
+end
+
+for f in (:+, :-)
+	@eval Base.$f(x::T, y::T) where {T<:Union{Odd, Even}} = Even($f(x.x, y.x))
+	@eval Base.$f(x::AbstractOddEvenInteger, y::AbstractOddEvenInteger) = Odd($f(x.x, y.x))
+end
+for f in (:*,)
+	@eval Base.$f(x::Odd, y::Odd) = Odd($f(x.x, y.x))
+	@eval Base.$f(x::AbstractOddEvenInteger, y::AbstractOddEvenInteger) = Even($f(x.x, y.x))
+end
+for f in (:-, :checked_abs)
+	@eval Base.$f(x::Odd) = Odd(Base.$f(x.x))
+	@eval Base.$f(x::Even) = Even(Base.$f(x.x))
+end
+
+Base.iseven(x::Odd) = false
+Base.isodd(x::Odd) = true
+Base.iseven(x::Even) = true
+Base.isodd(x::Even) = false
+
+Base.zero(x::Odd) = zero(x.x)
+Base.iszero(x::Odd) = false
+
+Base.show(io::IO, @nospecialize(x::AbstractOddEvenInteger)) = print(io, x.x)
+
+# HalfIntegers interface
+
+HalfIntegers.twice(x::AbstractOddEvenInteger) = Even(twice(x.x))
+
+const HalfOddInteger = HalfIntegers.Half{<:Odd{<:Integer}}
+const HalfEvenInteger = HalfIntegers.Half{<:Even{<:Integer}}
+
+for f in (:+, :-)
+	@eval begin
+		Base.$f(x::HalfOddInteger, y::Integer) = half(Odd($f(twice(x), twice(y))))
+		Base.$f(x::Integer, y::HalfOddInteger) = half(Odd($f(twice(x), twice(y))))
+		Base.$f(x::HalfEvenInteger, y::Integer) = half(Even($f(twice(x), twice(y))))
+		Base.$f(x::Integer, y::HalfEvenInteger) = half(Even($f(twice(x), twice(y))))
+	end
+end
+
+Base.:(==)(x::HalfOddInteger, y::Integer) = false
+Base.:(==)(y::Integer, x::HalfOddInteger) = false
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,23 @@
-using HalfOddIntegers
+using OddEvenIntegers
 using HalfIntegers
 using Test
 
 using Aqua
 @testset "project quality" begin
-    Aqua.test_all(HalfOddIntegers)
+    Aqua.test_all(OddEvenIntegers)
 end
 
-@testset "HalfOddIntegers.jl" begin
-    @testset "OddInt" begin
-        @test_throws Exception OddInt(2)
-        x = OddInt(3)
-        @test x isa OddInt
+@testset "OddEvenIntegers.jl" begin
+    @testset "Odd" begin
+        @test_throws Exception Odd(2)
+        x = Odd(3)
+        @test x isa Odd{Int}
         @test x == 3
         @test isodd(x)
         @test !iseven(x)
         @test !iszero(x)
         @test -x == -3
-        @test OddInt(x) == x
+        @test Odd(x) == x
         @test x == x == Int(x)
         @test !(x < x)
         @test !(x > x)
@@ -25,6 +25,9 @@ end
         @test x >= x
         @test (x >> 1) == 1
         @test (x << 1) == 6
+        @test iseven(x + x)
+        @test x + x == 6
+        @test x - x == 0
         @test x * x == 9
         @test abs(-x) == abs(x) == 3
         @test Base.checked_abs(-x) == 3
@@ -34,10 +37,49 @@ end
         @test div(x, x) == 1
         @test x + 2.0 == 5.0
         @test x + big(typemax(Int)) == big(3) + big(typemax(Int))
+        @test twice(x) == 6
+    end
+    @testset "Even" begin
+        @test_throws Exception Even(1)
+        x = Even(4)
+        @test x isa Even{Int}
+        @test x == 4
+        @test !isodd(x)
+        @test iseven(x)
+        @test !iszero(x)
+        @test -x == -4
+        @test Even(x) == x
+        @test x == x == Int(x)
+        @test !(x < x)
+        @test !(x > x)
+        @test x <= x
+        @test x >= x
+        @test (x >> 1) == 2
+        @test (x << 1) == 8
+        @test iseven(x + x)
+        @test x + x == 8
+        @test x - x == 0
+        @test x * x isa Even
+        @test x * x == 16
+        @test abs(-x) == abs(x) == 4
+        @test Base.checked_abs(-x) == 4
+        @test zero(x) == Even(0) == 0
+        @test trailing_zeros(x) == 2
+        @test rem(x, x) == 0
+        @test div(x, x) == 1
+        @test x + 2.0 == 6.0
+        @test x + big(typemax(Int)) == big(4) + big(typemax(Int))
+        @test twice(x) == 8
+    end
+    @testset "Odd and Even" begin
+        @test Odd(1) + Even(2) == Even(2) + Odd(1) == 3
+        @test isodd(Odd(1) + Even(2))
+        @test iseven(Odd(1) * Even(2))
+        @test Odd(1) * Even(2) == 2
     end
     @testset "HalfOddInt" begin
-        x = half(OddInt(3))
-        @test x isa Half{OddInt}
+        x = half(Odd(3))
+        @test x isa Half{Odd{Int}}
         @test x == 3/2
         @test !isinteger(x)
         @test -x == -3/2
@@ -49,7 +91,7 @@ end
         @test x - x == 0
         @test x * x == (3/2)^2
         @test x / x ≈ 1
-        y = half(OddInt(5))
+        y = half(Odd(5))
         @test x + y == 4
         @test x - y == -1
         @test x != 1
