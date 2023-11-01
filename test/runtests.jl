c20c82c790aef5804f825199f371057b2ad71d93
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-11-01 20:12:50 +0530

    Don't test Project.toml formatting on old versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,8 @@ using Test
 
 using Aqua
 @testset "project quality" begin
-    Aqua.test_all(OddEvenIntegers)
+    Aqua.test_all(OddEvenIntegers,
+        project_toml_formatting = VERSION >= v"1.9")
 end
 
 using Documenter
